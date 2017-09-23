14fa49a2c9a7078f7f308ae49ec802fac17c5899
artisin <te.artisin@gmail.com>
2017-09-23 18:10:02 -0500

    feat(__scripts__): add .nvmrc → #4
    CLOSES #4

---
+++ __scripts__/setup/generate-files/nvmrc.generate.js
@@ -0,0 +1,29 @@
+const fs   = require('fs-extra');
+const path = require('path');
+
+/**
+ * Configs the .nvmrc file
+ */
+const config = function ({dir, nvmrc}) {
+  let data = fs.readFileSync(path.resolve(__dirname, './template/.nvmrc'));
+  if (data) {
+    data = data.toString();
+    data = data.replace(/<VERSION>/g, Number(nvmrc));
+
+    //write out file
+    fs.writeFileSync(`${dir}.nvmrc`, data);
+  }
+};
+
+
+/**
+ * Example input
+ */
+// config({
+//   dir: './__scripts__/setup/__tests__/',
+//   nvmrc: '8.5.0'
+// });
+
+
+module.exports = config;
+
